5315ecf10658470cc61be916fb78aaa678be2177
zubairzia0 <zubairzia0@gmail.com>
2018-03-23 19:44:05 -0400

    stuff I learned today on variadic functions and passing values using float 64

---
+++ Functions/variadic.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main () {
+  n := average (3, 10)
+  fmt.Println (n)
+  
+}
+
+ func average (sf ...float64)  float64 {
+ // fmt.Println (sf)
+ // fmt.Printf ("%T \n" , sf)
+  
+  var total float64
+  for _, v := range sf {
+      total += v
+  }
+  return total / float64 (len(sf)) //len = length of sf
+  
+}
