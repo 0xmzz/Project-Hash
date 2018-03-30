0d2dee0ac130fc21f62eba47ba4be38f91e9b7d8
zubairzia0 <zubairzia0@gmail.com>
2018-03-30 01:26:23 -0400

    learning recursion and callbacks

--- Functions/return.go
+++ Functions/return.go
@@ -2,17 +2,21 @@ package main
 
 import "fmt"
 
-func wrapper () func() int {
-    var x int
-  return func () int {
-    x++
-    return x
-  }
+func main () {
+  
+  data := [] float64 {40960, 3432100}
+  n := divide (data)
+  fmt.Println(n)
   
 }
 
-func main () {
-  increment := wrapper ()
-  fmt.Println(increment ())
-  fmt.Println(increment())
-}
+//func ticket () float64 { 
+  
+ // v := 40960
+ // b := 3432100
+  //poolsize := 40960  // Every 144 blocks (~12 hours), the stake difficulty algorithm calculates a new Ticket Price in an attempt to keep the Ticket Pool size near the target pool size of 40,960 tickets. This 144 block window is referred to as the StakeDiffWindowSize.
+    //POSvalue := 3432100
+//  fmt.Println(v)
+//}
+//return v/float64 ()
+
